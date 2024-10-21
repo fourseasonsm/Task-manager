614644cca0b88c6441a12027a231db3dd6697b3b
renataburganova <rburganova1@gmail.com>
2024-10-21 20:09:32 +0300

    [renata] Add a registration window

--- Task_manager/loginwindow.cpp
+++ Task_manager/loginwindow.cpp
@@ -1,4 +1,5 @@
 #include "loginwindow.h"
+#include "registrationwindow.h"
 #include <QMessageBox>
 #include <QVBoxLayout>
 #include <QPushButton>
@@ -9,6 +10,7 @@ LoginWindow::LoginWindow(QWidget *parent)
     : QDialog(parent)
     , authenticated(false)
 {
+    // Поле логина
     QVBoxLayout *layout = new QVBoxLayout(this);
     QLabel *loginLabel = new QLabel("Логин:", this);
     layout->addWidget(loginLabel);
@@ -16,6 +18,7 @@ LoginWindow::LoginWindow(QWidget *parent)
     loginLineEdit = new QLineEdit(this);
     layout->addWidget(loginLineEdit);
 
+    // Поле пароля
     QLabel *passwordLabel = new QLabel("Пароль:", this);
     layout->addWidget(passwordLabel);
 
@@ -23,9 +26,16 @@ LoginWindow::LoginWindow(QWidget *parent)
     passwordLineEdit->setEchoMode(QLineEdit::Password); //ввод пароля не виден
     layout->addWidget(passwordLineEdit);
 
+    // Кнопка входа
     QPushButton *authLoginButton = new QPushButton("Войти", this);
     layout->addWidget(authLoginButton);
     connect(authLoginButton, &QPushButton::clicked, this, &LoginWindow::on_authLoginButton_clicked);
+
+    // Кнопка регистрации
+    QPushButton *regButton = new QPushButton("Зарегистрироваться", this);
+    layout->addWidget(regButton);
+    connect(regButton, &QPushButton::clicked, this, &LoginWindow::on_regButton_clicked);
+
     setLayout(layout);
 }
 
@@ -33,16 +43,26 @@ LoginWindow::~LoginWindow()
 {
 }
 
+// Нажатие на кнопку для перехода к окну регистрации
+void LoginWindow::on_regButton_clicked()
+{
+    RegistrationWindow *registerWindow = new RegistrationWindow(this);
+
+    registerWindow->show(); // Отображается поверх оккна логина, можно потом пофиксить
+}
+
 bool LoginWindow::isAuthenticated() const
 {
     return authenticated;
 }
 
+// Проверка логина и пароля
 void LoginWindow::on_authLoginButton_clicked()
 {
     QString login = loginLineEdit->text();
     QString password = passwordLineEdit->text();
 
+    // Текущие логин и пароль для входа 111 и 111
     if (login == "111" && password == "111") {
         authenticated = true;
         accept();
--- Task_manager/mainwindow.cpp
+++ Task_manager/mainwindow.cpp
@@ -4,42 +4,14 @@ MainWindow::MainWindow(QWidget *parent) : QWidget(parent) {
     setWindowTitle("Main Window");
     QVBoxLayout *mainLayout = new QVBoxLayout(this);
 
+    // Белая шапка
     QLabel *topPart = new QLabel();
     topPart->setStyleSheet("background-color: white;");
     topPart->setFixedHeight(50);
     topPart->setAlignment(Qt::AlignCenter);
     mainLayout->addWidget(topPart);
 
-    QLabel *overlay_usr = new QLabel("Пользователи онлайн", this);
-    overlay_usr->setStyleSheet("background-color: rgba(0, 0.5, 0, 0);");
-    overlay_usr->setFixedSize(150, 20);
-    overlay_usr->move(554, 35);
-    overlay_usr->raise();
-    QLabel *overlay_prf = new QLabel("Профиль", this);
-    overlay_prf->setStyleSheet("background-color: rgba(0, 0.5, 0, 0);");
-    overlay_prf->setFixedSize(150, 20);
-    overlay_prf->move(45, 35);
-    overlay_prf->raise();
-    QLabel *overlay_tsk = new QLabel("Текущие задачи", this);
-    overlay_tsk->setStyleSheet("background-color: rgba(0, 0.5, 0, 0);");
-    overlay_tsk->setFixedSize(150, 20);
-    overlay_tsk->move(300, 35);
-    overlay_tsk->raise();
-
-
-    /*QLabel *topleftStripe = new QLabel();
-    topleftStripe->setStyleSheet("background-color: lightgreen;");
-    mainLayout->addWidget(topleftStripe);
-    QLabel *topcenterStripe = new QLabel();
-    topcenterStripe->setStyleSheet("background-color: white;");
-    mainLayout->addWidget(topcenterStripe);
-    QLabel *toprightStripe = new QLabel();
-    toprightStripe->setStyleSheet("background-color: lightgreen;");
-    mainLayout->addWidget(toprightStripe);
-    mainLayout->setStretch(0, 1);
-    mainLayout->setStretch(1, 3);
-    mainLayout->setStretch(2, 1);*/
-
+    // Разделитель
     QFrame *separator = new QFrame();
     separator->setFrameShape(QFrame::HLine);
     separator->setFrameShadow(QFrame::Sunken);
@@ -47,22 +19,54 @@ MainWindow::MainWindow(QWidget *parent) : QWidget(parent) {
     separator->setStyleSheet("color: black;");
     mainLayout->addWidget(separator);
 
-    QHBoxLayout *bottomLayout = new QHBoxLayout();
-    QLabel *bottomleftStripe = new QLabel();
-    bottomleftStripe->setStyleSheet("background-color: lightgreen;");
-    bottomLayout->addWidget(bottomleftStripe);
-    QLabel *bottomcenterStripe = new QLabel();
-    bottomcenterStripe->setStyleSheet("background-color: white;");
-    bottomLayout->addWidget(bottomcenterStripe);
-    QLabel *bottomrightStripe = new QLabel(this);
-    bottomrightStripe->setStyleSheet("background-color: lightgreen;");
-    bottomLayout->addWidget(bottomrightStripe);
+    QHBoxLayout *bottomLayout = new QHBoxLayout(); // Горизонтальный слой
+
+    // Зеленая полоса слева
+    QLabel *bottomLeftStripe = new QLabel(this);
+    bottomLeftStripe->setStyleSheet("background-color: lightgreen;");
+    bottomLeftStripe->setAlignment(Qt::AlignTop | Qt::AlignCenter);
+    bottomLayout->addWidget(bottomLeftStripe);
+
+    QVBoxLayout *leftStripeLayout = new QVBoxLayout(bottomLeftStripe); // Вертикальный слой для левой полосы
+
+    // Текст "Профиль"
+    QLabel *overlay_prf = new QLabel("Профиль", bottomLeftStripe);
+    overlay_prf->setStyleSheet("background-color: transparent; color: black; font-size: 16px;");
+    leftStripeLayout->addWidget(overlay_prf, 0, Qt::AlignTop | Qt::AlignHCenter);
+
+    // Белая центральная полоса
+    QLabel *bottomCenterStripe = new QLabel(this);
+    bottomCenterStripe->setStyleSheet("background-color: white;");
+    bottomCenterStripe->setAlignment(Qt::AlignTop | Qt::AlignLeft);
+    bottomLayout->addWidget(bottomCenterStripe);
+
+    QVBoxLayout *centerStripeLayout = new QVBoxLayout(bottomCenterStripe); // Вертикальный слой для центральной полосы
+
+    // Текст "Текущие задачи"
+    QLabel *overlay_tsk = new QLabel("Текущие задачи", bottomCenterStripe);
+    overlay_tsk->setStyleSheet("background-color: transparent; color: black; font-size: 16px;");
+    centerStripeLayout->addWidget(overlay_tsk, 0, Qt::AlignTop | Qt::AlignLeft);
+
+    // Зеленая полоса справа
+    QLabel *bottomRightStripe = new QLabel(this);
+    bottomRightStripe->setStyleSheet("background-color: lightgreen;");
+    bottomRightStripe->setAlignment(Qt::AlignTop | Qt::AlignCenter);
+    bottomLayout->addWidget(bottomRightStripe);
+
+    QVBoxLayout *rightStripeLayout = new QVBoxLayout(bottomRightStripe); // Вертикальный слой для правой полосы
+
+    // Текст "Пользователи онлайн"
+    QLabel *overlay_usr = new QLabel("Пользователи онлайн", bottomRightStripe);
+    overlay_usr->setStyleSheet("background-color: transparent; color: black; font-size: 16px;");
+    rightStripeLayout->addWidget(overlay_usr, 0, Qt::AlignTop | Qt::AlignHCenter);
 
     // Устанавливаем размеры полос
-    bottomLayout->setStretch(0, 2);
-    bottomLayout->setStretch(1, 5);
-    bottomLayout->setStretch(2, 2);
+    bottomLayout->setStretch(0, 2); // Зелёная полоса слева
+    bottomLayout->setStretch(1, 5); // Белая центральная полоса
+    bottomLayout->setStretch(2, 2); // Зелёная полоса справа
     mainLayout->addLayout(bottomLayout);
+
+    setLayout(mainLayout); // Устанавливаем главный слой для окна
 }
 
 
